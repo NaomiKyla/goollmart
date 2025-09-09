8a6915bd98cc366bc33cf0976c60b07a71320305
naomi kyla <naomikyla@nomay-2.local>
2025-09-09 15:59:29 +0700

    Tambah unit test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class MainViewTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('/')
+        self.assertTemplateUsed(response, 'main.html')  # sesuai path template kamu
+
+    def test_context_data(self):
+        response = Client().get('/')
+        self.assertContains(response, "GoollMart")  
+        self.assertContains(response, "2406434102")  # NPM kamu
+        self.assertContains(response, "Naomi Kyla Zahra Siregar")  # Nama kamu
+        self.assertContains(response, "PBP B")  # Kelas kamu
