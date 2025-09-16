732d075422b193e2b81a8f827ffa52274f50054a
naomi kyla <naomikyla@nomay-2.local>
2025-09-17 01:21:55 +0700

    Tugas Individu 3

--- GoollMart/settings.py
+++ GoollMart/settings.py
@@ -147,4 +147,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+# Folder untuk menampung semua static files setelah collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
